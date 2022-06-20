db6b0941a61c57278b6e9a78f52cfb1d6cb3f481
DarpanCybercom <darpan.cybercom@gmail.com>
2022-06-20 02:54:58 -0600

    update

--- src/addAuth.js
+++ src/addAuth.js
@@ -3,9 +3,11 @@ const inquirer  = require('inquirer');
 const fs = require("fs");
 const path = require("path");
 const  { exec }   = require("child_process");
+const TerminalLog = require("./colors");
+
 
 const ConfigFileName = "appConfig.json";
-const ConfigFilePath = path.join(__dirname,'config',ConfigFileName);
+const ConfigFilePath = path.join('config',ConfigFileName); 
 
 const CheckConfigFile = () => {
 
@@ -16,27 +18,58 @@ const CheckConfigFile = () => {
     return true;
 };
 
+
+const AskForAuthType = async () => {
+
+    var questions = [];
+    var answer;
+
+    questions.push({
+        type: 'list',
+        name: 'authType',
+        message: 'Select Auth Type',
+        choices: ['Basic Auth', 'Basic Auth With Table' ,'Basic Auth With OAuth2', 'OAuth2']
+    });
+
+
+    do {
+        answers = await inquirer.prompt(questions);
+     } while (answers.authType == "");
+ 
+ 
+     return  answers.authType;
+
+
+
+}
+
+
+
 module.exports.addAuth = async () => {
 
-    console.log("File Upload Module Intializing.....");
-    console.log("File Upload Module Intialized");
-   
+    TerminalLog.info("Adding Auth");
+    
     var IsFileAvailable = CheckConfigFile();
 
     if(!IsFileAvailable){
 
-        console.log("Config File Not Found");
+    TerminalLog.error("Config File Not Found");
         return;
     }
-
-    console.log("Config File Found");
-
+  
     var configFile = fs.readFileSync(ConfigFilePath);
     var config = JSON.parse(configFile);
 
-    console.log("Config File Loaded");
 
-    console.log(config.AppConfig.apiFolderName);
+    var authType = await AskForAuthType();
+
+    TerminalLog.debug(authType);
+
+    
+
+
+
+
 
     
 };
